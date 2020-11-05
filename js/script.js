a3e61cf40132e9117ec20eb0ba307a0189fdb8a2
Vitaly <bulin.vitaly2013@yandex.kz>
2020-11-06 02:08:32 +0600

    финальная версия

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      infinite: true,
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 7000,
+      speed: 1000,
+    });
+  });
